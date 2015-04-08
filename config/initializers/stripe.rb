099aef0b0a3570f00b55e115192239dd46a2aa5b
Ravi Ramdass <ravi.ramdass@gmail.com>
2015-04-08 00:38:08 +0000

    Add users controller

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => ['nNMpq7JQQuRGfWNjRxaIcq0S'],
-  :secret_key      => ['tCJmRrV24f6GT2DN9xZKiPeD']
+  :publishable_key => ['pk_test_0Zjpzi55MGccc7GR36o6DPZv'],
+  :secret_key      => ['sk_test_tCJmRrV24f6GT2DN9xZKiPeD']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
